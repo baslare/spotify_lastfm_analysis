7640f2d27efe732a325cb7fbcc9b5628f3469d12
efe <efe.baslar@gmail.com>
2022-02-23 17:40:12 +0100

    finished the relevant python part

--- spofity_py.py
+++ spofity_py.py
@@ -11,6 +11,7 @@ import pandas as pd
 import json
 import requests
 import base64
+import urllib.parse
 
 
 class spotify:
@@ -37,7 +38,7 @@ class spotify:
                                 data = params)
         
         self.token = response.json().get("access_token")
-        
+            
     def search(self,artist="",track=""):
         
 
@@ -47,6 +48,7 @@ class spotify:
         
         
         q = f"{track} +artist:{artist}"
+        
         
         headers = {"Authorization": f"Bearer {self.token}",
                    "Content-Type":"application:json"}
@@ -61,6 +63,111 @@ class spotify:
                                     )
         
         return(response)
+    
+    def get_features_multiple_tracks(self,tracks):
+        
+        url = "https://api.spotify.com/v1/audio-features"
+        
+        track_string = ",".join(tracks)
+        
+        headers = {"Authorization": f"Bearer {self.token}",
+                   "Content-Type":"application:json"}
+        
+        params = {"ids":track_string}
+        
+        response_ = self.req_ses.get(url=url,
+                                 headers=headers,
+                                 params=params)
+        
+        return(response_)
+    
+    
+    def get_info_multiple_tracks(self,tracks):
+        
+        url = "https://api.spotify.com/v1/tracks"
+        track_string = ",".join(tracks)
+        
+        headers = {"Authorization": f"Bearer {self.token}",
+                   "Content-Type":"application:json"}
+        
+        params = {"ids":track_string}
+        
+        response_ = self.req_ses.get(url=url,
+                                 headers=headers,
+                                 params=params)
+        
+        return(response_)
+    
+    def get_info_multiple_artists(self,artists):
+        
+        
+        
+        url = "https://api.spotify.com/v1/artists"
+        artist_string = ",".join(artists)
+        
+        headers = {"Authorization": f"Bearer {self.token}",
+                   "Content-Type":"application:json"}
+        
+        params = {"ids":artist_string}
+        
+        response_ = self.req_ses.get(url=url,
+                                 headers=headers,
+                                 params=params)
+        
+        
+        return(response_)
+    
+    @staticmethod
+    def search_get_df(result_row):
+        unpack_tracks = result_row.get("tracks")
+        track_count = unpack_tracks.get("total")
+        track_query = unpack_tracks.get("href")
+        
+        if track_count > 0:
+            unpack_items = unpack_tracks.get("items")[0]
+            
+            
+            track_id = unpack_items.get("id")
+            track_name = unpack_items.get("name")
+            track_popularity = unpack_items.get("popularity")
+            track_duration = unpack_items.get("duration")
+            
+            unpack_album = unpack_items.get("album")
+            unpack_artist = unpack_items.get("artists")
+            artists_id = [x.get("id") for x in unpack_artist]
+            artists_name = [x.get("name") for x in unpack_artist]
+
+            album_id = unpack_album.get("id")
+            album_name = unpack_album.get("name")
+            album_rd = unpack_album.get("release_date")
+            
+            df_dict = {"track_id":track_id,
+                       "track_name":track_name,
+                       "track_popularity":track_popularity,
+                       "track_duration":track_duration,
+                       "artists_id":[artists_id],
+                       "artists_name":[artists_name],
+                       "album_id":album_id,
+                       "album_name":album_name,
+                       "album_rd":album_rd,
+                       "track_query":track_query}
+            return(pd.DataFrame(df_dict))
+        else:
+            df_dict = {"track_id":[None],
+                       "track_name":[None],
+                       "track_popularity":[None],
+                       "track_duration":[None],
+                       "artists_id":[None],
+                       "artists_name":[None],
+                       "album_id":[None],
+                       "album_name":[None],
+                       "album_rd":[None],
+                       "track_query":track_query}
+            return(pd.DataFrame(df_dict))
+        
+        
+        
+        
     
    
         
--- spotify_process.py
+++ spotify_process.py
@@ -6,39 +6,223 @@ Created on Tue Feb 22 12:44:00 2022
 """
 
 import os
-
-os.chdir("C:/Users/Efe/Desktop/Projeler/spotify_lastfm")
 from api_wrapper_wip import lfm_api
 from spofity_py import spotify
 import time
 import pandas as pd
 import json
 import requests
+import pickle
+import urllib.parse
+import numpy as np
 
 
+
+###change the working directory / if needed
+os.chdir("C:/Users/Efe/Desktop/Projeler/spotify_lastfm_analysis")
+
+
+
+
+
+###bring in the last.fm dataset
 lfm_df = pd.read_json("last_fm_raw.json")
+lfm_df_unique = lfm_df.drop_duplicates(subset=["artist.name","name"])
+lfm_df_unique.reset_index(inplace=True)
 
 
+###authenticate yourself with the spotify api
 sp = spotify(API_KEY="your_api_key",API_SECRET="your_api_secret")
 sp.auth()
 
 
-lfm_df_unique = lfm_df.drop_duplicates(subset=["artist.name","name"])
 
 
-asd = sp.search(lfm_df_unique["artist.name"].iloc[0],lfm_df_unique["name"].iloc[0])
-
+### search spotify, use last-fm scrobbles to create a personal list of songs played
 
 result_list = []
 
 i = 0
 
+temp_holder = []
+lfm_df_unique.loc[:,"track_query"] = ""
 
-while i < 50:
+while i < len(lfm_df_unique['artist.name']):
     
-    result_list.append(sp.search(lfm_df_unique["artist.name"].iloc[i],lfm_df_unique["name"].iloc[i],lfm_df_unique["album.#text"].iloc[i]))
     
+    response_ = sp.search(lfm_df_unique["artist.name"].iloc[i],lfm_df_unique["name"].iloc[i])
+    lfm_df_unique["track_query"].iloc[i] = response_.request.path_url
+    
+    if(response_.status_code == 200):
+        temp_holder.append(response_)
+    elif(response_.status_code == 429):
+        print(response_.status_code)
+        break
+    
+    if i % 50 == 0:
+        
+        print(f"{i+1} of {len(lfm_df_unique['artist.name'])}")
+        result_list = result_list + temp_holder
+        temp_holder = []
+        time.sleep(2)
+        
     i +=1
     
+    
+result_list = result_list + temp_holder
 
-result_list[5].json()
+
+#save the data
+with open("spot.pickle", "wb") as f:
+            pickle.dump(result_list, f, protocol=pickle.HIGHEST_PROTOCOL)
+         
+
+#reload the dataset if needed                 
+#with open("spot.pickle", "rb") as f:
+#            result_list = pickle.load(f)
+    
+            
+###tidy up the data
+            
+
+result_list = [x.json() for x in result_list]           
+result_list_df = [sp.search_get_df(x) for x in result_list]
+result_list_df = pd.concat(result_list_df)
+result_list_df.reset_index(inplace=True)
+
+master_df = pd.concat([result_list_df,lfm_df_unique],axis=1)
+
+result_list_exists = result_list_df[[x != None for x in result_list_df.track_id]]
+result_list_exists.reset_index(inplace=True)
+result_list_exists.drop(["index"],inplace=True, axis=1)
+
+
+
+###get audio features of the tracks - 100 per query
+interval_1 = np.arange(0,result_list_exists.shape[0],100)
+interval_2 = np.arange(99,result_list_exists.shape[0],100)
+interval_2 = np.append(interval_2,result_list_exists.shape[0]-1)
+
+
+features_list = []
+
+for x,y in zip(interval_1,interval_2):
+    rspnse = sp.get_features_multiple_tracks(result_list_exists.loc[x:y,"track_id"])
+    features_list.append(rspnse)
+ 
+    
+#save the data
+with open("spot_features.pickle", "wb") as f:
+            pickle.dump(features_list, f, protocol=pickle.HIGHEST_PROTOCOL)
+            
+
+#reload the dataset if needed
+#with open("spot_features.pickle", "rb") as f:
+#            features_list = pickle.load(f)
+    
+features_list = [x.json() for x in features_list]
+features_list = [x.get("audio_features") for x in features_list]
+features_list = list(np.concatenate(features_list))
+features_list = [pd.DataFrame(x,    index=[0]) for x in features_list]
+features_df = pd.concat(features_list)
+features_df.reset_index(inplace=True)
+
+###get track info - 50 per query
+
+interval_1 = np.arange(0,result_list_exists.shape[0],50)
+interval_2 = np.arange(49,result_list_exists.shape[0],50)
+interval_2 = np.append(interval_2,result_list_exists.shape[0]-1)
+
+
+info_list = []
+
+for x,y in zip(interval_1,interval_2):
+    rspnse = sp.get_info_multiple_tracks(result_list_exists.loc[x:y,"track_id"])
+    info_list.append(rspnse)
+    
+info_list = [x.json() for x in info_list]
+info_list = [x.get("tracks") for x in info_list]
+info_list = list(np.concatenate(info_list))
+
+
+###get artist info - 50 per query
+
+
+artist_id = list(result_list_exists["artists_id"])
+artist_id = np.concatenate(artist_id)
+artist_id = list(artist_id)
+
+artist_list = []
+
+
+interval_1 = np.arange(0,len(artist_id),50)
+interval_2 = np.arange(49,len(artist_id),50)
+interval_2 = np.append(interval_2,len(artist_id)-1)
+
+for x,y in zip(interval_1,interval_2):
+    rspnse = sp.get_info_multiple_artists(artist_id[x:y+1])
+    artist_list.append(rspnse)
+    
+#save the data
+with open("spot_artists.pickle", "wb") as f:
+            pickle.dump(artist_list, f, protocol=pickle.HIGHEST_PROTOCOL)
+            
+            
+
+artist_list = [x.json() for x in artist_list]
+artist_list = [x.get("artists") for x in artist_list]
+artist_list = list(np.concatenate(artist_list))
+artist_list = [pd.json_normalize(x) for x in artist_list]
+artist_df = pd.concat(artist_list)
+            
+            
+
+
+### merge all into one master df
+
+#get rid of useless columns
+features_df = features_df.drop(["index","type","uri","track_href","analysis_url"],  axis=1)
+features_df = features_df.rename(columns={"id":"track_id"})
+
+#create master_df
+            
+master_df = pd.concat([result_list_df,lfm_df_unique],axis=1)
+master_df = master_df.drop_duplicates(subset=["artist.name","name"])
+master_df = master_df[[x != None for x in result_list_df.track_id]]
+
+
+#simplify artists
+master_df = master_df.assign(artist_name = [x[0] for x in list(master_df["artists_name"])])
+master_df = master_df.assign(artist_id = [x[0] for x in list(master_df["artists_id"])])
+master_df = master_df.drop(["index","track_duration","level_0","index"],  axis=1)
+master_df.reset_index(inplace=True, drop=True)
+
+###even though the most prudent way would be to just left_join, but some spotify track ids correspond to multiple last.fm tracks
+###which makes sense, because we searched for some song-artist title combinations.
+#master_df = pd.merge(master_df,features_df,how="left",left_on="track_id",right_on="id")
+master_df = pd.concat([master_df,features_df],axis=1)
+
+artist_df = artist_df[["id","genres","followers.total","popularity"]]
+artist_df = artist_df.rename(columns={"id":"artist_id"})
+artist_df = artist_df.drop_duplicates(subset="artist_id")
+
+master_df = master_df.merge(artist_df,how="left",on="artist_id")
+
+ft_col = features_df.columns.to_list()
+ft_col.remove("track_id")
+master_df = master_df[["track_id","track_name","album_id","album_name","album_rd","artist_id","artist_name","name","artist.name","track_query","track_popularity","artists_id","artists_name"]+ft_col]
+master_df = master_df.loc[:,~master_df.columns.duplicated()]
+
+#populate the playlist record with spotify characteristics
+
+lfm_df = lfm_df.merge(master_df,how="left",on=["name","artist.name"])
+lfm_df.reset_index(inplace=True, drop=True)
+lfm_df = lfm_df[[not pd.isna(x) for x in lfm_df.track_id]]
+
+
+
+with open("spot_lfm_final.json", "w",encoding="utf-8") as file:
+            lfm_df.to_json(file, force_ascii=False)
+
+    
+
